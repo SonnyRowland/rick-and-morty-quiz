2a6968f7a4cd32f2e3bade5c1e8e0e1952ee06f4
SonnyRowland <sonnyrowland@gmail.com>
2025-08-18 10:24:57 +0100

    improve difficulty select screen (#14)

--- constants.ts
+++ constants.ts
@@ -4,4 +4,10 @@ export const DIFFICULTY_MIN_EPISODES = {
   hard: 2,
 };
 
+export const difficultyDescriptions = {
+  easy: "Beginner level - only an idiot could get this wrong",
+  medium: "Prepare to struggle - your knowledge will be tested",
+  hard: "Hardcore mode - brace yourself for a challenge",
+};
+
 export const TOTAL_QUESTIONS = 10;
--- screens/DifficultyScreen.tsx
+++ screens/DifficultyScreen.tsx
@@ -1,10 +1,15 @@
-import { Text } from "react-native";
 import { Button, ButtonText } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { VStack } from "@/components/ui/vstack";
+import { Text } from "react-native";
 
 import { useGame } from "../context/GameContext";
-import { NavigationProp } from "../types";
+import { NavigationProp } from "@/types";
+import { difficultyDescriptions } from "@/constants";
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { useCallback } from "react";
+import { ScreenWrapper } from "@/components/ScreenWrapper";
+import { Box } from "@/components/ui/box";
 
 export const DifficultyScreen = () => {
   const navigation = useNavigation<NavigationProp>();
@@ -17,21 +22,39 @@ export const DifficultyScreen = () => {
   );
 
   return (
-    <>
-      <Text>Select difficulty</Text>
-      <Text>Current difficulty: {difficulty}</Text>
-      <Button onPress={() => setDifficulty("easy")}>
-        <ButtonText>Easy</ButtonText>
-      </Button>
-      <Button onPress={() => setDifficulty("medium")}>
-        <ButtonText>Medium</ButtonText>
-      </Button>
-      <Button onPress={() => setDifficulty("hard")}>
-        <ButtonText>Hard</ButtonText>
-      </Button>
+    <ScreenWrapper>
+      <Heading>Select difficulty</Heading>
+      <VStack style={{ gap: "20" }}>
+        <Button
+          onPress={() => setDifficulty("easy")}
+          variant={difficulty === "easy" ? "solid" : "outline"}
+        >
+          <ButtonText>Easy</ButtonText>
+        </Button>
+        <Button
+          onPress={() => setDifficulty("medium")}
+          variant={difficulty === "medium" ? "solid" : "outline"}
+        >
+          <ButtonText>Medium</ButtonText>
+        </Button>
+        <Button
+          onPress={() => setDifficulty("hard")}
+          variant={difficulty == "hard" ? "solid" : "outline"}
+        >
+          <ButtonText>Hard</ButtonText>
+        </Button>
+      </VStack>
+
+      <Box style={{ height: 60 }}>
+        <Text style={{ textAlign: "center", marginVertical: 20 }}>
+          {difficultyDescriptions[difficulty]}
+        </Text>
+      </Box>
+
       <Button onPress={() => navigation.navigate("Trivia")}>
         <ButtonText>Start Game</ButtonText>
+        <ButtonText style={{ color: "white" }}>→</ButtonText>
       </Button>
-    </>
+    </ScreenWrapper>
   );
 };
